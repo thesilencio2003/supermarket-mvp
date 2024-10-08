cd21a07e6bdfcdbe1b2d1e40cf7f2780c3cb30f9
Alejandro ibarra <alejohola23@gmail.com>
2024-10-07 23:28:23 -0500

    initial Project

---
+++ SQLQuery1.sql
@@ -0,0 +1,10 @@
+﻿use Supermarket
+create table PayMode
+(
+	Pay_Mode_Id int identity (100000,1) primary key,
+	Pay_Mode_Name nvarchar (50) not null,
+	Pay_Mode_Observation nvarchar (50) not null,
+)
+go
+insert into PayMode values('Cash','Cash mode')
+insert into PayMode values('Credit card','Credit card mode')
