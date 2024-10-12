286a81aacfade96608bef29bbbf068e8d654b9f0
Alejandro ibarra <alejohola23@gmail.com>
2024-10-12 18:54:04 -0500

    CustomersModel

---
+++ Models/CustomersModel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace Supermarket_mvp.Models
+{
+    internal class CustomersModel
+    {
+
+        [DisplayName("Customers Id")]
+        public int Id { get; set; }
+        [DisplayName("Customers Document")]
+        [Required(ErrorMessage = "Customers Document is required")]
+        [StringLength(16, ErrorMessage = "Customers Document must be 16 characters or less")]
+        public string Document { get; set; }
+
+        [DisplayName("Customers Name")]
+        [Required(ErrorMessage = "Customers Name is required")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Customers name must be between 3 and 50 characters")]
+        public string Name { get; set; }
+        [DisplayName("Customers firstName")]
+        [Required(ErrorMessage = "Customers firstName is required")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Customers FirstName must be between 3 and 50 characters")]
+        public string FirstName { get; set; }
+        [DisplayName("Customers lastName")]
+        [Required(ErrorMessage = "Customers lastName is required")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Customers LastName must be between 3 and 50 characters")]
+        public string lastName { get; set; }
+
+        [DisplayName("Customers Address")]
+        [Required(ErrorMessage = "Customers Address is required")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Customers Address must be between 3 and 50 characters")]
+        public string Address {  get; set; }
+        [DisplayName("Customers Date")]
+        [Required(ErrorMessage = "Customers Date is required ")]
+
+        public string Date { get; set; }
+        [DisplayName("Customers Phone")]
+        [Required(ErrorMessage = "Customers Date is required ")]
+        public string PhoneNumber { get; set; }
+        [DisplayName("Customers Email")]
+        [Required(ErrorMessage = "Customers Email is required ")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Customers Phone must be between 3 and 50 characters")]
+        public string Email { get; set; }
+    }
+}
--- SQLQuery2.sql
+++ SQLQuery2.sql
@@ -10,6 +10,7 @@ Product_Stock int not null,
 go
 insert into Product values('soap',100,100)
 
+drop table Categories
 create table Categories
 (
 Categories_Id int identity (100000,1) primary key,
@@ -18,3 +19,18 @@ Categories_Description nvarchar (50) not null,
 )
 go
 insert into Categories values('Cleaning','This cleaning personal')
+
+drop table Customers
+create table Customers
+(
+Customers_Id int identity (10000,1) primary key,
+Customers_Document char(15) not null unique,
+Customers_FirstName nvarchar (50) not null,
+Customers_LastName nvarchar (50) not null,
+Customers_Address nvarchar (50) not null,
+Customers_Birthday date,
+Customers_Phone char(16),
+Customers_Email nvarchar (100) not null
+)
+go
+insert Customers values(1004563221,'Juan','Pablo','Calle olvido','1990-02-28','1234567890','juanpablo@example.com')
