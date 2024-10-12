5d61cb1b9f086407357fca88a0accc35f736f122
Alejandro ibarra <alejohola23@gmail.com>
2024-10-12 10:47:19 -0500

    Created ICategoriesView

--- Models/CategoriesModel.cs
+++ Models/CategoriesModel.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,17 @@ namespace Supermarket_mvp.Models
 {
     internal class CategoriesModel
     {
+
+        [DisplayName("Categories Id")]
+        public int Id { get; set; }
+
+        [DisplayName("Categories Name")]
+        [Required(ErrorMessage = "Categories name is required")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Categories name must be between 3 and 50 characters")]
+        public string Name { get; set; }
+        [DisplayName("Description")]
+        [Required(ErrorMessage = "Categories Description is required")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Categories Description must be between 3 and 50 characters")]
+        public string Description { get; set; }
     }
 }
